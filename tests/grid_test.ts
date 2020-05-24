eaf74450d06a2f71db51425f9286dd62aec19628
tyrelh <krankyjoe@gmail.com>
2020-05-23 17:02:50 -0700

    add updateCell and outOfBounds unit tests

--- tests/grid_test.ts
+++ tests/grid_test.ts
@@ -2,15 +2,14 @@ import { assert } from "https://deno.land/std/testing/asserts.ts";
 import { Grid } from "../app/grid.ts";
 import { GameRequest } from "../app/types.ts";
 import { FOOD } from "../app/keys.ts";
+import { newCell } from "../app/utils.ts";
 
 
-Deno.test("Root response contains expected fields", () => {
+Deno.test("Grid initGrid and buildGrid", () => {
     // given
     const grid = new Grid(mockGameRequest.board.width, mockGameRequest.board.height);
-
     // when
     grid.buildGrid(mockGameRequest);
-
     //then
     assert(grid.height === mockGameRequest.board.height);
     assert(grid.data.length === mockGameRequest.board.height);
@@ -19,6 +18,54 @@ Deno.test("Root response contains expected fields", () => {
     for (let food of mockGameRequest.board.food) {
         assert(grid.value(food) === FOOD);
     }
+});
+
+
+Deno.test("Grid updateCell", () => {
+    // given
+    const grid = new Grid(mockGameRequest.board.width, mockGameRequest.board.height);
+    grid.buildGrid(mockGameRequest);
+    const testValue = 55;
+    const testCell = newCell(5, 5);
+    // when
+    grid.updateCell(testCell, testValue);
+    // then
+    assert(grid.value(testCell) === testValue);
+    assert(grid.data[testCell.y][testCell.x] === testValue);
+    assert(grid.value(newCell(5, 4)) !== testValue);
+});
+
+
+Deno.test("Grid outOfBounds", () => {
+    // given
+    const grid = new Grid(mockGameRequest.board.width, mockGameRequest.board.height);
+    grid.buildGrid(mockGameRequest);
+    const cell1 = newCell(-1, 1);
+    const cell2 = newCell(1, -1);
+    const cell3 = newCell(-1, -1);
+    const cell4 = newCell(mockGameRequest.board.width, 1);
+    const cell5 = newCell(1, mockGameRequest.board.height);
+    const cell6 = newCell(mockGameRequest.board.width, mockGameRequest.board.height);
+    const cell7 = newCell(1, 1);
+    const cell8 = newCell(mockGameRequest.board.width - 1, mockGameRequest.board.height - 1);
+    // when
+    const outOfBounds1 = grid.outOfBounds(cell1);
+    const outOfBounds2 = grid.outOfBounds(cell2);
+    const outOfBounds3 = grid.outOfBounds(cell3);
+    const outOfBounds4 = grid.outOfBounds(cell4);
+    const outOfBounds5 = grid.outOfBounds(cell5);
+    const outOfBounds6 = grid.outOfBounds(cell6);
+    const outOfBounds7 = grid.outOfBounds(cell7);
+    const outOfBounds8 = grid.outOfBounds(cell8);
+    // then
+    assert(outOfBounds1);
+    assert(outOfBounds2);
+    assert(outOfBounds3);
+    assert(outOfBounds4);
+    assert(outOfBounds5);
+    assert(outOfBounds6);
+    assert(!outOfBounds7);
+    assert(!outOfBounds8);
 });
 
 
