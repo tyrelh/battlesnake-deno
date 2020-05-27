2ee89c2f3d36cc3c24be7b58c0974e02b98830ef
tyrelh <krankyjoe@gmail.com>
2020-05-26 18:52:40 -0700

    saved move json for a future test. won't pass this move now but is a good one to test for

--- tests/move_test.ts
+++ tests/move_test.ts
@@ -12,6 +12,7 @@ Deno.test("Should take danger head on collision instead of dead end", () => {
   assert(result.move === expectedMove);
 });
 
+
 Deno.test("Two dangerous snakes converging on the left move", () => {
   // given
   const gameRequest = {"game":{"id":"94feded6-26c7-4a9a-b9a3-570831fced0d","timeout":500},"turn":36,"board":{"height":11,"width":11,"food":[{"x":1,"y":1},{"x":2,"y":2}],"snakes":[{"id":"gs_9mgCVvHRD7QkPc6TCBcYMFbC","name":"SnickerSnek","health":78,"body":[{"x":0,"y":2},{"x":0,"y":3},{"x":1,"y":3},{"x":1,"y":2}],"head":{"x":0,"y":2},"length":4,"shout":""},{"id":"gs_rjMFrTGVqJhQ6Bg7q6C7qJvb","name":"hissin-bastid","health":97,"body":[{"x":8,"y":8},{"x":7,"y":8},{"x":6,"y":8},{"x":5,"y":8},{"x":5,"y":7},{"x":4,"y":7},{"x":3,"y":7},{"x":2,"y":7}],"head":{"x":8,"y":8},"length":8,"shout":""},{"id":"gs_FGrrMq96fmR3KQRjFFrVQp3D","name":"snacky","health":90,"body":[{"x":7,"y":5},{"x":6,"y":5},{"x":5,"y":5},{"x":4,"y":5},{"x":3,"y":5},{"x":2,"y":5},{"x":1,"y":5},{"x":0,"y":5}],"head":{"x":7,"y":5},"length":8,"shout":""},{"id":"gs_gvx4VB8KdchgmRtWHf9xvfMS","name":"giraffe-snek","health":64,"body":[{"x":8,"y":6},{"x":7,"y":6},{"x":6,"y":6}],"head":{"x":8,"y":6},"length":3,"shout":""},{"id":"gs_CmkCbPbHKMg3Hq7Yv889vch8","name":"Zero Cool Deno Snake Local","health":64,"body":[{"x":9,"y":7},{"x":9,"y":6},{"x":9,"y":5}],"head":{"x":9,"y":7},"length":3,"shout":""}]},"you":{"id":"gs_CmkCbPbHKMg3Hq7Yv889vch8","name":"Zero Cool Deno Snake Local","health":64,"body":[{"x":9,"y":7},{"x":9,"y":6},{"x":9,"y":5}],"head":{"x":9,"y":7},"length":3,"shout":""}}
@@ -27,4 +28,5 @@ Deno.test("Two dangerous snakes converging on the left move", () => {
 
 
 
-
+// tricky one, two snakes to the right in a small area
+const badMove = {"game":{"id":"331040cf-5c68-4ac5-a8a9-4e32f5ab54ef","timeout":500},"turn":67,"board":{"height":11,"width":11,"food":[{"x":7,"y":1},{"x":9,"y":9},{"x":9,"y":0}],"snakes":[{"id":"gs_qMmQ7pmrCbXwfFbp3HHpGgD9","name":"Hungry Snake","health":88,"body":[{"x":7,"y":2},{"x":7,"y":3},{"x":7,"y":4},{"x":7,"y":5},{"x":6,"y":5},{"x":6,"y":6},{"x":6,"y":7},{"x":5,"y":7},{"x":4,"y":7},{"x":4,"y":8}],"head":{"x":7,"y":2},"length":10,"shout":""},{"id":"gs_gYYGMrJxkTdkbdGHb9rFbPVH","name":"snake-2020","health":93,"body":[{"x":9,"y":2},{"x":9,"y":3},{"x":9,"y":4},{"x":9,"y":5},{"x":9,"y":6},{"x":8,"y":6},{"x":8,"y":7}],"head":{"x":9,"y":2},"length":7,"shout":""},{"id":"gs_MTcY9cdPFTcbtvF4DTGJHyjB","name":"Zero Cool Deno Snake Local","health":96,"body":[{"x":6,"y":1},{"x":6,"y":0},{"x":5,"y":0},{"x":4,"y":0},{"x":4,"y":1},{"x":5,"y":1},{"x":5,"y":2},{"x":4,"y":2}],"head":{"x":6,"y":1},"length":8,"shout":""}]},"you":{"id":"gs_MTcY9cdPFTcbtvF4DTGJHyjB","name":"Zero Cool Deno Snake Local","health":96,"body":[{"x":6,"y":1},{"x":6,"y":0},{"x":5,"y":0},{"x":4,"y":0},{"x":4,"y":1},{"x":5,"y":1},{"x":5,"y":2},{"x":4,"y":2}],"head":{"x":6,"y":1},"length":8,"shout":""}};
